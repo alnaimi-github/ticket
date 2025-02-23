f1ef202fc2799c89f0e0c8ccfd7467bb12d2d778
alnaimi-github <alnymybdalwas1@gmail.com>
2025-02-23 22:46:13 +0300

    feat: add new ticket component and integrate it into the tickets dashboard

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,9 +1,12 @@
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [
+    NewTicketComponent
+  ],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
