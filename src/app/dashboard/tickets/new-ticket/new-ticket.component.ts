d04a550c9965ed412ac74744e52ff61dcb61d904
alnaimi-github <alnymybdalwas1@gmail.com>
2025-02-28 20:07:39 +0300

    sothing change

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../../shared/button/button.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent,FormsModule],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
+  host: {
+    "name":'textarea',
+    "(click)": "onClick()"
+  }
 })
-export class NewTicketComponent {
+export class NewTicketComponent  {
+  onClick() {
+    console.log("hii");
+  }
+sub(input:string,form:HTMLFormElement) {
+console.log(input);
+form.reset();
+}
+
 
 }
